dae7ad8e144cfa766b34f5b2903f6f28aef23030
Anita Ilieva <anita.ilieva@acando.no>
2018-02-26 13:19:58 +0100

    Set up firebase

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
+import firebase from 'firebase';
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { Header } from 	'./components/common/Header';
 
 class App extends Component {
+
+	componentWillMount() {
+	firebase.initiliazeApp({
+    apiKey: 'AIzaSyCsbrFyRxWQkEGEzqirib4Iq7D2pNV6i6w',
+    authDomain: 'authentication-251ea.firebaseapp.com',
+    databaseURL: 'https://authentication-251ea.firebaseio.com',
+    projectId: 'authentication-251ea',
+    storageBucket: 'authentication-251ea.appspot.com',
+    messagingSenderId: '1038159040112'
+ });
+}
 	render() {
 		return (
 			<View>
-				<Header headerText="Authentication" />
+				<Header headerText='Authentication' />
 				<Text>An app</Text>
 			</View>
 		);
